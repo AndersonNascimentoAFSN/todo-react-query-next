083c4ced6c7cdb924ce3520f445cd8c5e1805cf9
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-04-24 14:32:29 -0300

    refactor

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,16 +1,16 @@
 'use client'
 
-type HeaderProps = {
-  status?: number
-}
+import { useTodoList } from "@/hooks/useTodoList";
 
-export function Header({ status }: HeaderProps) {
+export function Header() {
+  const { data: todo } = useTodoList();
+
   return (
     <header className="w-screen bg-fuchsia-800 p-10">
       <h1 className="m-auto text-white text-3xl">Lista de Tarefas</h1>
       {/* <h3 className="m-auto text-white text-xl">Organize sua vida de forma simples e prática</h3> */}
       <span className="text-white mr-3">Quantidade de tarefas:</span>
-      <span className="text-white">{status}</span>
+      <span className="text-white">{todo?.length}</span>
     </header>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
-'use client'
-
-import { useTodoList } from "@/hooks/useTodoList";
 import { Header } from "./components/Header";
 import { Todo } from "./components/Todo";
 import { TodoList } from "./components/TodoList";
 
 export default function Home() {
-  const { data: todo } = useTodoList();
 
   return (
     <main>
-      <Header status={todo?.length} />
+      <Header />
       <Todo />
 
       <TodoList />
