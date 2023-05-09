7b6fc91f4b5c6b3688244e2d86a58d232302c84d
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-05-09 10:40:39 -0300

    wip: remove files unnecessary

--- src/utils/handleSetColorByGender.ts
+++
@@ -1,8 +0,0 @@
-export function handleSetColorByGender(gender: 'male' | 'female') {
-  const genders = {
-    male: 'text-green-500',
-    female: 'text-pink-500'
-  }
-
-  return genders[gender]
-}
