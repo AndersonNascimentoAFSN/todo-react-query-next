82bead39edb613e4e0f7c7bce0850dea0aa3b536
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-05-09 15:34:23 -0300

    wip: refactor use query functions

--- src/hooks/useTodoById.ts
+++ src/hooks/useTodoById.ts
@@ -5,9 +5,20 @@ import { TodoService } from "../services/http/todo";
 export function useTodoById({ id }: { id: string }) {
   const oneMinute = 1000 * 60
 
-  return useQuery(['todoList', id], () => TodoService.getTodoById({ id }),
-    {
-      staleTime: oneMinute * 30,
-    }
+  return useQuery({
+    queryKey: ['todoList', id],
+    queryFn: () => TodoService.getTodoById({ id }),
+    staleTime: oneMinute * 30
+  },
   )
 }
+
+// export function useTodoById({ id }: { id: string }) {
+//   const oneMinute = 1000 * 60
+
+//   return useQuery(['todoList', id], () => TodoService.getTodoById({ id }),
+//     {
+//       staleTime: oneMinute * 30,
+//     }
+//   )
+// }
--- src/hooks/useTodoList.ts
+++ src/hooks/useTodoList.ts
@@ -1,14 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 
-// import { getTodoList } from "@/services/todo";
 import { TodoService } from "../services/http/todo";
+
 
 export function useTodoList() {
   const oneMinute = 1000 * 60
 
-  return useQuery(['todoList'], () => TodoService.getTodoList(),
-    {
-      staleTime: oneMinute * 30,
-    }
+  return useQuery({
+    queryKey: ['todoList'],
+    queryFn: TodoService.getTodoList,
+    staleTime: oneMinute * 30
+  },
   )
 }
+
+// export function useTodoList() {
+//   const oneMinute = 1000 * 60
+
+//   return useQuery(['todoList'], () => TodoService.getTodoList(),
+//     {
+//       staleTime: oneMinute * 30,
+//     }
+//   )
+// }
